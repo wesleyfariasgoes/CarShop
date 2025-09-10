948e6d351efd79caf3c878c2d358438d20eb95ad
wesleygoes <wfariasgoes@gmail.com>
2025-09-10 19:44:10 -0300

    Initial commit

---
+++ app/src/main/java/com/wfghc/carshop/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.wfghc.carshop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/wfghc/carshop/ui/feature/intro/IntroScreen.kt
@@ -0,0 +1,95 @@
+package com.wfghc.carshop.ui.feature.intro
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wfghc.carshop.R
+
+@Composable
+@Preview
+fun IntroScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 48.dp, bottom = 32.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text(
+                    text = "Easy Way to buy\n your dream car",
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(start = 24.dp)
+                        .statusBarsPadding(),
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(height = 24.dp))
+                Text(
+                    text = "Lorem Ipsum is simply dummy text " +
+                            "of the printing and typesetting industry. \n" +
+                            "Lorem Ipsum has been the industry's standard dummy text ever since \n" +
+                            "the 1500s, when an unknown printer took a galley of type and scrambled\n it to " +
+                            "make a type specimen book. It has survived not only five centuries, but also\n " +
+                            "the leap into electronic typesetting, remaining essentially unchanged. ",
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp)
+                        .statusBarsPadding(),
+                    fontSize = 14.sp,
+                    lineHeight = 24.sp
+                )
+            }
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.intro_car),
+                    contentDescription = "Car",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(min = 200.dp)
+                )
+                Spacer(modifier = Modifier.height(height = 32.dp))
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(height = 56.dp)
+                        .padding(horizontal = 24.dp)
+
+                ) {
+                    Text(text = "Iniciar",
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold)
+                }
+            }
+        }
+    }
+}
